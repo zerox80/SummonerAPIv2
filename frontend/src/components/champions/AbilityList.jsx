bd7a201ec899c05e6427dda1f3cbebe02598ebf1
zerox80 <rujbin@proton.me>
2025-10-03 19:42:58 +0200

    fix

---
+++ frontend/src/components/champions/AbilityList.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import Tooltip from '../Tooltip.jsx';
+import '../../styles/champions/champion-abilities.css';
+
+const KEY_MAP = ['Q', 'W', 'E', 'R'];
+
+export default function AbilityList({ passive, spells }) {
+  return (
+    <section className="champion-abilities glass-panel">
+      <header className="champion-abilities__header">
+        <h3>Fähigkeitenübersicht</h3>
+        <p>Verstehe das Kit und die Spielweise des Champions.</p>
+      </header>
+      <div className="champion-abilities__grid">
+        {passive && (
+          <article className="ability-card ability-card--passive">
+            <div className="ability-card__icon">
+              <img src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/passive/${passive.image?.full}`} alt={passive.name} loading="lazy" />
+            </div>
+            <div className="ability-card__content">
+              <div className="ability-card__meta">
+                <span className="ability-card__hotkey">Passive</span>
+                <h4>{passive.name}</h4>
+              </div>
+              <p>{passive.description}</p>
+            </div>
+          </article>
+        )}
+        {spells?.map((spell, index) => (
+          <article key={spell.id} className="ability-card">
+            <div className="ability-card__icon">
+              <img src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/spell/${spell.image?.full}`} alt={spell.name} loading="lazy" />
+            </div>
+            <div className="ability-card__content">
+              <div className="ability-card__meta">
+                <span className="ability-card__hotkey">{KEY_MAP[index] || ''}</span>
+                <h4>{spell.name}</h4>
+                <Tooltip label="Cooldown">
+                  <span className="ability-card__cooldown">CD: {spell.cooldownBurn}</span>
+                </Tooltip>
+              </div>
+              <p>{spell.description}</p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+AbilityList.propTypes = {
+  passive: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.shape({
+      full: PropTypes.string
+    })
+  }),
+  spells: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    cooldownBurn: PropTypes.string,
+    image: PropTypes.shape({
+      full: PropTypes.string
+    })
+  }))
+};
